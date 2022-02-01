1a812b8ca29538995b6ce7cbcd9bba166d230e01
Greg Sithole <gregsithole@hotmail.co.za>
2022-02-01 19:53:22 +0200

    Fixed base Url and env usage

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,11 +1,11 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoApiHeaders = {
     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-    'x-rapidapi-key': process.env["REACT_APP_RAPIDAPI_KEY "]
+    'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
 }
 
-const baseUrl = 'https://coinranking1.p.rapidapi.com/coin/Qwsogvtv82FCd/exchanges';
+const baseUrl = 'https://coinranking1.p.rapidapi.com/';
 
 const createRequest = (url) => ({url, headers: cryptoApiHeaders})
 
