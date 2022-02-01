1e4993ec2b758a94d7c16238dc60ea660c5e79c9
Greg Sithole <gregsithole@hotmail.co.za>
2022-02-01 19:40:04 +0200

    Added CryptoApi.js

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,39 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': process.env["REACT_APP_RAPIDAPI_KEY "]
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com/coin/Qwsogvtv82FCd/exchanges';
+
+const createRequest = (url) => ({url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi
+
+// var options = {
+//     method: 'GET',
+//     url: '',
+//     params: {
+//         referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//         limit: '50',
+//         offset: '0',
+//         orderBy: '24hVolume',
+//         orderDirection: 'desc'
+//     },
+//     headers: {
+//
+//     }
+// };
