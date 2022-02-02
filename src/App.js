c2ce9455224bbddbb64606409cc3d34f010a5a65
Greg Sithole <gregsithole@hotmail.co.za>
2022-02-02 18:36:30 +0200

    Updated App.js and App.css

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ function App() {
                 <div className="routes">
                     <Routes>
                         <Route path="/" element={<HomePage/>}/>
-                        <Route path="/exchanges" element={<Exchanges/>}/>
                         <Route path="/cryptocurrencies" element={<Cryptocurrencies/>}/>
                         <Route path="/crypto/:coinId" element={<CryptoDetails/>}/>
                         <Route path="/news" element={<News/>}/>
@@ -29,7 +28,6 @@ function App() {
                 </Typography.Title>
                 <Space>
                     <Link to="/">Home</Link>
-                    <Link to="/exchanges">Exchanges</Link>
                     <Link to="/news">News</Link>
                 </Space>
             </div>
